e4231acc054650a7661a93a0a4e8415740357fe7
pasboy30 <parthps2522@gmail.com>
2018-11-19 12:31:30 -0500

    API token files

---
+++ credentials.py
@@ -0,0 +1,7 @@
+# Twitter API access tokens
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+
+# Register at https://developer.twitter.com/content/developer-twitter/en.html to get API tokens
