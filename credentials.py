100524e0fe6528fc644089bc0bdaa3c3f5629745
Parth Shukla <34710564+pasboy30@users.noreply.github.com>
2018-11-19 12:13:56 -0500

    Delete credentials.py

--- credentials.py
+++
@@ -1,7 +0,0 @@
-# Twitter API access tokens
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = ""
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-
-# To get those, get yourself registered at : https://developer.twitter.com/content/developer-twitter/en.html
