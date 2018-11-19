9531cd1cf7e3db0500f5414996c2c5b33c8a64ee
pasboy30 <parthps2522@gmail.com>
2018-11-19 12:18:53 -0500

    keywords based on which tweets will be streamed

---
+++ keywords.py
@@ -0,0 +1,23 @@
+#List of keywords related to cyber security
+
+keywords = ['XSS', 'trojan', 'cve', 'DNS', 'vulnerability', 'overflow',  'DDos', 'hijacking',
+            'SQL injection', 'breach', 'sniffer', 'cryptography', 'piggybacking', 'ciphertext', 'checksum', 'malware',
+           'firewall', 'infrastructure', 'buffer', 'PKCS', 'hacking', 'malicious', 'RSA',
+            'spoofing', 'phishing', 'social engineering',
+            'encryption', 'CERT', 'breach', 'leakage', 'spam', 'botnet', 'blackhat',
+            'brute force', 'exploit', 'honeypot', 'IP', 'ransomware', 'VPN', 'worm', 'Zero Day', 'Zombie', 'whitehat', 'cyber', 'cyberspace','IOT',
+            'password','bug bounty','darkweb', 'deepweb', 'penetration testing','cyber attack' ]
+
+cybersecurity_specialists = [ 'EHackerNews']
+
+cybersecurity_companies = ['BitDefender', 'Malwarebytes', 'Onion_ID', 'moixsec', 'NakedSecurity', 'kaspersky', 'norton']
+
+cybersec_spec = ['redditstatus','twitchstatus','AdobeSecurity',
+                 'JuniperSecurity']
+
+#'web_security', 'intersecww', 'githubstatus','DOStatus',
+#'schneierblog','mikko','briankrebs','neiljrubenking','dangoodin001','gcluley',
+#'campuscodi','peterkruse' ,'e_kaspersky','troyhunt','SwiftOnSecurity', 'lcheylus', 'InfosecurityMag','TripwireInc','HackRead', 'BetaNews', 'CSOonline', 'SecurityWeek', 'DarkReading'
+
+
+
